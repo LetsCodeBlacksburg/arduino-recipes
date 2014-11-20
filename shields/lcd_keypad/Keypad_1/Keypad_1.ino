b5e2b4fc558451b40e8a0ca054e170d285dd814e
Eddie Sheffield <eddie.sheffield@rackspace.com>
2014-11-20 18:45:01 -0500

    Add lcd / keypad shield samples

---
+++ shields/lcd_keypad/Keypad_1/Keypad_1.ino
@@ -0,0 +1,37 @@
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
+// read the buttons
+int read_LCD_buttons()
+{
+ int adc_key_in = analogRead(0);      // read the value from the sensor
+ // These values should work with all current keypads.
+ // If you get unexpected results check the readings you get with you shield
+ if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 250)  return btnUP; 
+ if (adc_key_in < 450)  return btnDOWN; 
+ if (adc_key_in < 650)  return btnLEFT; 
+ if (adc_key_in < 850)  return btnSELECT;  
+
+ return btnNONE;  // when all others fail, return this...
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = read_LCD_buttons();
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1);        // delay in between reads for stability
+}
---
+++ shields/lcd_keypad/LCD_1/LCD_1.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+
+// Create the LiquidCrystal object and define the pins used on the LCD shield
+// The parameters are: rs, rw, enable, d4, d5, d6, d7
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+void setup()
+{
+ // Start the library, telling it we have a 16 column x 2 row display
+ lcd.begin(16, 2);
+ // Put the cursor on the first column of the first row
+ lcd.setCursor(0, 0);
+ lcd.print("Hello, world!");
+ // First column of second row
+ lcd.setCursor(0, 1);
+ lcd.print("Seconds: ");
+}
+ 
+void loop()
+{
+ lcd.setCursor(9,1);            // move cursor to second line "1" and 9 spaces over
+ lcd.print(millis()/1000);      // display seconds elapsed since power-up
+}
